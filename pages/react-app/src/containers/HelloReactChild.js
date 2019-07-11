a8426831f1740c409a0b41d61b2e72d0edbcdc02
PaulHan <zhangyu.han@outlook.com>
2019-07-12 04:20:45 +0800

    chore: state

--- pages/react-app/src/containers/HelloReactChild.js
+++ pages/react-app/src/containers/HelloReactChild.js
@@ -1,7 +1,20 @@
 import React, {Component} from 'react';
 
 export default class extends Component {
-    render() {
-        return <p>HelloReactChild</p>;
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: 0,
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({id: this.state.id + 1})
+    }
+
+    render(state = this.state) {
+        const {id} = state;
+        return <p onClick={this.handleClick}>HelloReactChild{id}</p>;
     }
 }
